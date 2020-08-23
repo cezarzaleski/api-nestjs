9bbd738fced0e4fda7914faad733d878ac9cb300
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2020-08-22 23:31:01 -0300

    feat(*): migrations de usuário

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
+// @ts-ignore
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      // @ts-ignore
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
+      logging: Boolean(process.env.TYPEORM_LOGGING),
+      // entities: [User, Room],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/migrations/1598149083747-CreateUsuario.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsuario1598149083747 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'tb_usuario',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                },
+                {
+                    name: 'senha',
+                    type: 'varchar',
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'CURRENT_TIMESTAMP'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('tb_usuario');
+    }
+
+}
